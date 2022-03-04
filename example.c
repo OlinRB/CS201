510af65ab6ed6f716484e09c60aab83aea4821c1
OlinRB <olinrb@live.com>
2022-03-03 22:29:46 -0500

    testing

--- example.c
+++ example.c
@@ -15,11 +15,11 @@ int done;
 void handler1(int signum) {
     if (signum == SIGUSR1) {
         printf("\nGot SIGUSR1\n");
-        printf("this is handler99(): got a signal %d\n", signum);
+        //printf("this is handler99(): got a signal %d\n", signum);
     }
     if (signum == SIGUSR2) {
         printf("\nGot SIGUSR2\n");
-        printf("this is handler2(): got a signal %d\n", signum);
+        //printf("this is handler2(): got a signal %d\n", signum);
     }
     done = 1;
 }
