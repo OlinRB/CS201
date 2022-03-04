bc53841154abc71bacf70f807c5026308a65516f
OlinRB <olinrb@live.com>
2022-03-03 22:06:59 -0500

    testing

--- example.c
+++ example.c
@@ -1,36 +1,40 @@
+// jdh 1-31-22
+// CS 201 S22
 //
-// Created by olinr on 3/3/2022.
-//
+// compile this and run it; then, in a second terminal window,
+// do this command: "kill -SIGUSR1 <pid>" with the pid that this
+// program prints out when it start
 
-// C program to implement sighup(), sigint()
-// and sigquit() signal functions
-#include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
-// function declaration
+int done;
+
 void handler1(int signum) {
     printf("this is handler1(): got a signal %d\n", signum);
+    done = 1;
 }
 
-
-// driver code
 int main() {
-    int pid = fork();
-    struct sigaction sa = {0};
-    sa.sa_handler = &handler1;
-    sigaction(SIGUSR1, &sa, NULL);
-    if (pid == -1)
-        return 1;
-    if (pid == 0)  {
-        printf("This is the child process, PID: %d", getpid());
-        //kill(getpid(), SIGUSR1);
-    } else {
-        printf("This is the parent process, PID: %d", getpid());
-        wait(NULL);
-    }
+    int pid;
+    struct sigaction action;
+
+    done = 0;
+    pid = getpid();
+
+    printf("Starting; my pid is %d\n", pid);
+
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = handler1;
+    sigaction(SIGUSR1, &action, NULL);
+
+    printf("now wait for something to happen\n");
+    kill(getpid(), SIGUSR1);
+    while ( ! done );
+
+    printf("done!\n");
     return 0;
 }
 
