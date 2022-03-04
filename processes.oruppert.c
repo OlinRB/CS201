3203646f55ddcce5efad100120e9483d575b7bbb
OlinRB <olinrb@live.com>
2022-03-03 21:15:54 -0500

    testing

--- processes.oruppert.c
+++ processes.oruppert.c
@@ -67,7 +67,7 @@ int main(int argc, char *argv[]) {
         }
         printf("I am the child, and I read this from the shared memory: '%s'\n", ptr);
         shmdt(ptr);
-        kill(getpid(), SIGUSR1);
+        kill(pid, SIGUSR1);
     }
 
     shmdt(ptr);
