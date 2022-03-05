7bab2cf5f2df49b60f54f90d96b8f162c688496f
OlinRB <olinrb@live.com>
2022-03-04 23:59:18 -0500

    testing

--- processes.oruppert.c
+++ processes.oruppert.c
@@ -54,22 +54,26 @@ int main() {
         return (8);
     }
 
-    // Set up signals
+//    // Set up signals
+//    if (pid > 0) {
+//        // Parent
+//        action.sa_handler = handler;
+//        sigaction(SIGUSR1, &action, NULL);
+//        printf("\nI am the parent and my pid is: %d\n", getpid());
+//    } else {
+//        // Child
+//        pid = getpid();
+//        action.sa_handler = handler;
+//        printf("I am the child and my pid is %d\n", pid);
+//        sigaction(SIGUSR2, &action, NULL);
+//    }
+
+    run = 1;
     if (pid > 0) {
         // Parent
         action.sa_handler = handler;
         sigaction(SIGUSR1, &action, NULL);
         printf("\nI am the parent and my pid is: %d\n", getpid());
-    } else {
-        // Child
-        pid = getpid();
-        action.sa_handler = handler;
-        printf("I am the child and my pid is %d\n", pid);
-        sigaction(SIGUSR2, &action, NULL);
-    }
-
-    run = 1;
-    if (pid > 0) {
         while (run) {
             while (!done);
             done = 0;
@@ -89,6 +93,11 @@ int main() {
         }
         wait(NULL);
     } else {
+        // Child
+        pid = getpid();
+        action.sa_handler = handler;
+        printf("I am the child and my pid is %d\n", pid);
+        sigaction(SIGUSR2, &action, NULL);
         kill(parentPID, SIGUSR1);
         while (run) {
             while (!finished);
