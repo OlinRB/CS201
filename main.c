6d80f6fdc8c2b27bd135e387046fcc4befd56650
OlinRB <olinrb@live.com>
2022-04-28 19:33:35 -0400

    try main on silk

--- main.c
+++ main.c
@@ -171,14 +171,14 @@ int insertWord(FILE *fp, char *word) {
             // Go to end of file to write word
             setFile(fp, filesize);
             fwrite(&inputWord, sizeof(inputWord), 1, fp);
-//            // Determine where word starts
-//            filesize = checkFileSize(fp);
-//            long long wordStarts = filesize - (MAXWORDLEN + 1);
-//            // Go to the end of the file to write the pointer to the next word
-//            setFile(fp, filesize);
-//            // Write pointer as 0
-//            long long ptr = 0;
-//            num = fwrite(&ptr, sizeof(long long), 1, fp);
+            // Determine where word starts
+            filesize = checkFileSize(fp);
+            long long wordStarts = filesize - (MAXWORDLEN + 1);
+            // Go to the end of the file to write the pointer to the next word
+            setFile(fp, filesize);
+            // Write pointer as 0
+            long long ptr = 0;
+            num = fwrite(&ptr, sizeof(long long), 1, fp);
 //            // Now write starting location of word at letter location within first 26 bytes
 //            setFile(fp, 0);
 //            fwrite(&wordStarts, letterIndex, 1, fp);
