50216bb3eaf588e2b77e30a3f0bea616babbd48f
OlinRB <olinrb@live.com>
2022-04-29 17:32:01 -0400

    try main on silk

--- main.c
+++ main.c
@@ -430,7 +430,7 @@ int main() {
     insertWord(fp, "aword");
     printFileData(fp);
     int cnt;
-    char testLetter = 'A';
+    char testLetter = 'n';
     countWords(fp, testLetter, &cnt);
     printf("\nThere are %d words with the letter %c\n", cnt, testLetter);
     //testUtils();
