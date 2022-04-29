a0ab09705630e37ce414d209d731a3d52067c684
OlinRB <olinrb@live.com>
2022-04-28 23:07:50 -0400

    try main on silk

--- main.c
+++ main.c
@@ -347,7 +347,7 @@ int main() {
     // Write word to file
     insertWord(fp, "nardles");
     insertWord(fp, "middle");
-    insertWord(fp, "nard0");
+    insertWord(fp, "nardo");
 
     //testUtils();
 
