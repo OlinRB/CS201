840dab48e97d6a70c77e25b6bbacdb959d6c964d
OlinRB <olinrb@live.com>
2022-04-28 23:35:00 -0400

    try main on silk

--- main.c
+++ main.c
@@ -254,7 +254,7 @@ int insertWord(FILE *fp, char *word) {
             Record newWord;
             fread(&newWord.word, sizeof(newWord.word), 1, fp);
             fread(&newWord.nextpos, sizeof (newWord.nextpos), 1, fp);
-            //printf("Read in next word | %s |\n", newWord.word);
+            printf("Read in next word | %s |\n", newWord.word);
 //            setFile(fp, newWord.nextpos);
 //            while (newWord.nextpos != 0) {
 //                fread(&newWord.word, sizeof(newWord.word), 1, fp);
