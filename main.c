26c0857a9f11941ac3fd90977451e8014d4b9482
OlinRB <olinrb@live.com>
2022-04-28 20:08:43 -0400

    try main on silk

--- main.c
+++ main.c
@@ -181,7 +181,7 @@ int insertWord(FILE *fp, char *word) {
             num = fwrite(&ptr, sizeof(long), 1, fp);
             // Now write starting location of word at letter location within first 26 bytes
             setFile(fp, 0);
-            fwrite(&wordStarts, letterIndex, 1, fp);
+            //fwrite(&wordStarts, letterIndex, 1, fp);
             // Set file back to start
             setFile(fp, 0);
 
